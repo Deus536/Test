ea16bfee282d01b7a2dcad2a40bd586188ae996c
AsusRog <Deus536@mail.ru>
2023-03-12 09:48:58 +0300

    Массивы

---
+++ src/lesson1/Array2.java
@@ -0,0 +1,32 @@
+package lesson1;
+
+import java.util.Arrays;
+
+public class Array2 {
+
+    public static void main(String [] args){
+        array();
+    }
+    public static void array() {
+        int[] array = {1, 2, 3, 4, 5, 6,};
+        int[] array2 = new int[array.length];
+        int number = 1;
+        if (number == 0) {
+            System.out.print(array.length + " ");
+            return;
+        }
+
+        for(int i = 0; i < array.length; i++) {
+            if (i + number > array.length - 1){
+                array2[i + number - array.length] = array[i];
+            } else {
+                array2[i + number] = array[i];
+            }
+
+        }
+        for(int x : array2){
+        System.out.print(x + " ");
+    }
+    }
+
+}
