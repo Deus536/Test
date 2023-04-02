b7fe239f7272de89f7ad8399545761e028861762
AsusRog <Deus536@mail.ru>
2023-04-02 10:50:35 +0300

    Basket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Basket basket = new Basket();
+        basket.add("мороженое", 10, 2, 1.5);
+        System.out.println(basket.getTotalPrice());
+        System.out.println(basket.getTotalWeight());
+        System.out.println(basket.contains("мороженое"));
+        basket.add("чупик", 2);
+        System.out.println(basket.getTotalPrice());
+        System.out.println(basket.getTotalWeight());
+        basket.add("кола",50, 3);
+        System.out.println(basket.getTotalPrice());
+        System.out.println(basket.getTotalWeight());
+        basket.add("вес", 4,55, 4.7);
+        System.out.println(basket.getTotalPrice());
+        System.out.println(basket.getTotalWeight());
+        basket.clear();
+        System.out.println(basket.getTotalPrice());
+        System.out.println(basket.getTotalWeight());
+        System.out.println(basket.contains("мороженое"));
     }
 }
