a5bc977a08dce79333acc5686d61e98ed96a3223
AsusRog <Deus536@mail.ru>
2023-04-09 09:28:09 +0300

    CargoTransportation1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import Encapsulation.CargoTransportation;
+import Encapsulation.Dimensions;
+
 public class Main {
     public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(12, 14, 5);
 
     }
 }
