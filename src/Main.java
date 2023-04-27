9519f3115ea18b57172346d9c887372c3f0ea1a7
Asus <deus536@mail.ru>
2023-04-27 17:16:50 +0300

    Time v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         Time.collectBirthday(1989,2,1);
+        Time.getPeriodBetweenDates(LocalDate.of(1922,2,3), LocalDate.of(1923,3,1));
     }
 }
--- src/Time.java
+++ src/Time.java
@@ -1,19 +1,26 @@
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.Locale;
+
 
 public class Time {
     public static void collectBirthday(int year, int month, int day){
         LocalDate localDate = LocalDate.now();
         LocalDate birthday = LocalDate.of(year,month,day);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        birthday.format(formatter);
         int count = 0;
-        if(localDate.isAfter(birthday)){
+        while (localDate.isAfter(birthday)){
             LocalDate nextBirthday = birthday.plusYears(1);
+            birthday = nextBirthday;
             count++;
-           System.out.println(count + " - " + nextBirthday + " - " +nextBirthday.getDayOfMonth() );
+           System.out.println(count + " - " + nextBirthday.format(formatter) + " - " + nextBirthday.getDayOfWeek().getDisplayName(TextStyle.SHORT,Locale.ENGLISH));
         }
+    }
+    public static void  getPeriodBetweenDates(LocalDate firstDate, LocalDate secondDate){
+
+        Period p = Period.between(firstDate, secondDate);
+        System.out.println("Years: " + p.getYears() + ", Months: " +  p.getMonths() + ", Days: "  + p.getDays());
     }
 }
