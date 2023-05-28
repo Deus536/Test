3509e061a771cf3e0d7f0a29573b8010d78d030f
Asus <deus536@mail.ru>
2023-05-28 08:29:27 +0300

    Книга v3

--- src/TelephoneBook.java
+++ src/TelephoneBook.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,25 +9,51 @@ public class TelephoneBook {
     public static void getBook() throws IOException {
         TreeMap<String, String> map = new TreeMap<>() {
         };
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        Pattern pattern = Pattern.compile("[а-яА-Я]{1,}");
-        Pattern pattern1 = Pattern.compile("\\d[0-9]");    //(\+7)(\d[0-9]{10})+/g
+        Pattern patternName = Pattern.compile("^[а-яА-Я]+$");
+        Pattern patternNumber = Pattern.compile("(\\+7)(\\d[0-9]{9})");    //(\+7)(\d[0-9]{10})+/g
+//        Pattern patternBug = Pattern.compile("^[\\wа-яА-Я,\" .\\-=`~@:;\\{\\}\\]\\[]+$");
         while (true) {
             System.out.println("Введите команду, номер телефона или имя: ");
             String str = bufferedReader.readLine();
-            Matcher matcher = pattern.matcher(str);
-            Matcher matcher1 = pattern1.matcher(str);
-            if (matcher.find()) {
+            Matcher matcherName = patternName.matcher(str);
+            Matcher matcherNumber = patternNumber.matcher(str);
+//            Matcher matcherBug = patternBug.matcher(str);
+            String commands = "LIST";
+            if (str.equals(commands)) {
+                if (!map.isEmpty()) {
+                    System.out.println("Список всех контактов: \nИмя - " + map.keySet() + ". Номер телефона - " + map.values());
+                } else {
+                    System.out.println("Список пуст");
+                }
+//                boolean presence = false;
+//                for (String key : map.keySet()) {
+//                    Integer value = Integer.valueOf(map.get(key));
+//                    System.out.println("Список всех контактов: \nИмя - " + key + ". Номер телефона - " + value);
+//                    presence = true;
+//                }
+//                if (!presence) {
+//                    System.out.println("Список пуст.");
+//                }
+            }
+            if (matcherName.find()) {
                 if (map.containsKey(str)) {
-                    map.get(str);
+                    for (Map.Entry entry : map.entrySet()){
+                        if(entry.getKey().equals(str)){
+
+                        }
+                    }
+                        map.get(str);
                     System.out.println(str + " - " + map.get(str));
                 } else {
-                    System.out.println("Введите телефон: ");
+                    System.out.println("Введите телефон для абаннента \"" + str + "\" :");
                     String tel = bufferedReader.readLine();
                     while (true) {
-                        Matcher matcherTel = pattern1.matcher(tel);
+                        Matcher matcherTel = patternNumber.matcher(tel);
                         if (matcherTel.find()) {
                             map.put(str, tel);
+                            System.out.println("Контанкт сохранен!");
                             break;
                         } else {
                             System.out.println("Вы ввели не правильный номер.");
@@ -38,7 +62,7 @@ public class TelephoneBook {
                     }
                 }
             }
-            if (matcher1.find()) {
+            if (matcherNumber.find()) {
                 boolean presence = false;
                 for (Map.Entry entry : map.entrySet()) {
                     if (entry.getValue().equals(str)) {
@@ -47,12 +71,13 @@ public class TelephoneBook {
                     }
                 }
                 if (!presence) {
-                    System.out.println("Введите имя: ");
+                    System.out.println("Введите имя для номера \"" + str + "\" :");
                     String name = bufferedReader.readLine();
                     while (true) {
-                        Matcher matcherName = pattern.matcher(name);
-                        if (matcherName.find()) {
+                        Matcher matcherName1 = patternName.matcher(name);
+                        if (matcherName1.find()) {
                             map.put(name, str);
+                            System.out.println("Контанкт сохранен!");
                             break;
                         } else {
                             System.out.println("Ввели не правильно имя");
@@ -61,7 +86,9 @@ public class TelephoneBook {
                     }
                 }
             }
-
         }
     }
 }
+
+
+
